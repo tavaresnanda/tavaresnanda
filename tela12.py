5d59d8f7bac9d88d12c02974aa290ea03c5afb5d
tavaresnanda <fernandatavaresdossantos77@gmail.com>
2025-03-13 08:24:38 -0300

    Create tela12.py

---
+++ tela12.py
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLOC</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>Meu Blog</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="posts">
+            <article class="post">
+                <h2>Título 1</h2>
+                <p>Publicado em <strong>12 de Março de 2025</strong></p>
+                <p>Esse é o primeiro post que eu crio em março de 2025</p>
+                <a href="" class="btn">Post Completo</a>
+
+            </article>
+
+            <article class="post">
+                <h2>título 2</h2>
+                <p>Publicado em <strong>13 de Março de 2025</strong></p>
+                <p>Esse é o quarto post que foi criado em 13/03/2025</p>
+                <a href="" class="btn">Post Completo</a>
+            </article>
+
+
+            <article class="post">
+                <h2>título 3:</h2>
+                <p>Publicado em <strong>12 de Março de 2025</strong></p>
+                <p>Esse é o terceiro post que foi criado em 12/03/2025</p>
+                <a href="" class="btn">Post Completo</a>
+            </article>
+        </section>
+    </main>
+
+    <aside class="sidebar">
+        <h3>Quem sou eu</h3>
+        <p>Eu me chamo Maria Fernanda,tenho 18 anos,faço curso de desenvolvimento de sistemas,sou de São Mateus,ES.</p>
+    </aside>
+
+    <footer>
+        <p>&copy;BLOG - Desenvolvido por <strong>Maria Fernanda</strong></p>
+    </footer>
+
+
+</body>
+
+</html>
