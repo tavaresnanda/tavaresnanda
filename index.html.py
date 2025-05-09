467a3b397321f5bcbd2a861ed3aeff0b661e6e5b
tavaresnanda <fernandatavaresdossantos77@gmail.com>
2025-05-09 11:42:15 -0300

    index.html.py

---
+++ index.html.py
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Tela Nova</title>
+    <link rel="stylesheet" href="teste.css">
+</head>
+<body>
+    <div class="d1">
+    <h1>Logim</h1><p>
+        <input type="email" placeholder="E-mail"></p>
+        <input type="password" placeholder="Senha">
+    </div>
+
+    <div class="d2">
+        <h1>div2</h1><p>
+    </div>
+
+</body>
+</html>
