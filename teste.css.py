924210132b53cded5c406214ce92e280e50e3667
tavaresnanda <fernandatavaresdossantos77@gmail.com>
2025-05-09 11:41:29 -0300

    teste.css.py

---
+++ teste.css.py
@@ -0,0 +1,22 @@
+*{
+    padding: auto;
+}
+
+.d1{
+    background-color: white;
+    padding: 40;
+    border-radius: 12px;
+    box-shadow: 0 10px 40px rgba(0, 0, 0,0.1);
+    width: 100%;
+    max-width: 360px;
+    text-align: center;
+
+}
+
+body{
+    background-color: #f5f7f7;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    height: 100vh;
+}
